daa56daf51184b43ce275675c090f53b1825e61a
firasel <mdrasel601390@gmail.com>
2023-02-15 23:04:07 +0600

    Setup project structure

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<div class="container">
+		<h1 class="text-center text-[50px] font-medium leading-[57px] text-[#17AEA4]">About us</h1>
+	</div>
+</section>
---
+++ tailwind.config.cjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#17AEA4",
+        secondary: "#414042",
+        'black-light': "#333333",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "3rem",
+        xl: "4rem",
+        "2xl": "4rem",
+      },
+    },
+  },
+  plugins: [],
+}
