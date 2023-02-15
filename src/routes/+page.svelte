3019ae3f5fc3e402fbefc1dea804a4e6b8f672ef
firasel <mdrasel601390@gmail.com>
2023-02-16 01:23:43 +0600

    Border style create

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,10 @@
+<script>
+	import { inview } from 'svelte-inview';
+	import { scale } from 'svelte/transition';
+	let isVisible = false;
+	console.log(isVisible);
+</script>
+
 <svelte:head>
 	<title>About</title>
 	<meta name="description" content="Svelte demo app" />
@@ -41,26 +48,83 @@
 		<h1 class="text-center text-[50px] font-medium leading-[57px] text-[#17AEA4] mb-14">
 			How it Work
 		</h1>
-		<div class="grid grid-cols-1 md:grid-cols-3 gap-12 text-center relative">
-			<div class="px-8 py-20 border-[3px] rounded-[20px] border-primary">
-				<h2 class="text-[40px] font-medium leading-[46px] capitalize mb-9">Copy Long URL</h2>
-				<p class="text-[20px] font-light text-black leading-[34px] break-words">
-					https://www.thrdailystar.net/business/global-economy/news/german-industrial-output-falls-more-expected-3241836
-				</p>
+		<div
+			use:inview
+			on:enter={() => (isVisible = true)}
+			on:leave={() => (isVisible = false)}
+			class="grid grid-cols-1 md:grid-cols-3 gap-12 text-center relative"
+		>
+			<div class="px-8 py-20 border-[3px] rounded-[20px] border-primary relative">
+				{#if isVisible}
+					<!-- Dot -->
+					<span
+						in:scale={{ start: 0, duration: 50 }}
+						class="w-3 bg-primary aspect-square rounded-full absolute -top-7 -left-1 origin-center hidden md:block"
+					/>
+					<!-- Top border -->
+					<span
+						in:scale={{ start: 0, duration: 300, delay: 40 }}
+						class="border-[2px] border-dashed border-primary rounded-[15px] absolute -top-6 w-[calc(100%+22px)] left-0 origin-left hidden md:block"
+					/>
+				{/if}
+				<div>
+					<h2 class="text-[40px] font-medium leading-[46px] capitalize mb-9">Copy Long URL</h2>
+					<p class="text-[20px] font-light text-black leading-[34px] break-words">
+						https://www.thrdailystar.net/business/global-economy/news/german-industrial-output-falls-more-expected-3241836
+					</p>
+				</div>
 			</div>
-			<div class="px-8 py-20 border-[3px] rounded-[20px] border-primary">
-				<h2 class="text-[40px] font-medium leading-[46px] capitalize mb-14">
-					Paste into the sortener ber
-				</h2>
-				<p class="text-[20px] font-light text-black leading-[34px] break-words">
-					Random short: Teely.me/TreRB6T or Brand short: Teel.me /DIFT02023
-				</p>
+			<div class="px-8 py-20 border-[3px] rounded-[20px] border-primary relative">
+				{#if isVisible}
+					<!-- Left border -->
+					<span
+						in:scale={{ start: 0, duration: 350, delay: 340 }}
+						class="border-[2px] border-dashed border-primary rounded-[15px] absolute -left-6 h-[calc(100%+28px)] -top-4 origin-top hidden md:block"
+					/>
+					<!-- Bottom border -->
+					<span
+						in:scale={{ start: 0, duration: 300, delay: 690 }}
+						class="border-[2px] border-dashed border-primary rounded-[15px] absolute -bottom-6 w-[calc(100%+40px)] -left-5 origin-left hidden md:block"
+					/>
+					<!-- Right border -->
+					<span
+						in:scale={{ start: 0, duration: 350, delay: 990 }}
+						class="border-[2px] border-dashed border-primary rounded-[15px] absolute -right-6 h-[calc(100%+28px)] -top-4 origin-bottom hidden md:block"
+					/>
+				{/if}
+				<div>
+					<h2 class="text-[40px] font-medium leading-[46px] capitalize mb-14">
+						Paste into the sortener ber
+					</h2>
+					<p class="text-[20px] font-light text-black leading-[34px] break-words">
+						Random short: Teely.me/TreRB6T or Brand short: Teel.me /DIFT02023
+					</p>
+				</div>
 			</div>
-			<div class="px-8 py-20 border-[3px] rounded-[20px] border-primary">
-				<h2 class="text-[40px] font-medium leading-[46px] capitalize mb-14">track every click</h2>
-				<p class="text-[20px] font-light text-black leading-[34px] break-words">
-					Track every click on hourly, daily, monthly, and yearly basis.
-				</p>
+			<div class="px-8 py-20 border-[3px] rounded-[20px] border-primary relative">
+				{#if isVisible}
+					<!-- Top border -->
+					<span
+						in:scale={{ start: 0, duration: 300, delay: 1340 }}
+						class="border-[2px] border-dashed border-primary rounded-[15px] absolute -top-6 w-[calc(100%+22px)] -left-6 origin-left hidden md:block"
+					/>
+					<!-- Right arrow -->
+					<span
+						in:scale={{ start: 0, duration: 50, delay: 1640 }}
+						class="w-0 h-0 
+				border-t-[6px] border-t-transparent
+				border-l-[9px] border-l-primary
+				border-b-[6px] border-b-transparent
+				absolute -top-7 -right-2 origin-left hidden md:block
+				"
+					/>
+				{/if}
+				<div>
+					<h2 class="text-[40px] font-medium leading-[46px] capitalize mb-14">track every click</h2>
+					<p class="text-[20px] font-light text-black leading-[34px] break-words">
+						Track every click on hourly, daily, monthly, and yearly basis.
+					</p>
+				</div>
 			</div>
 		</div>
 	</div>
