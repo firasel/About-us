3bc96cba010f0d5e021088633740d768d82f5e83
firasel <mdrasel601390@gmail.com>
2023-02-15 23:53:55 +0600

    About us section create

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,38 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<div class="container">
-		<h1 class="text-center text-[50px] font-medium leading-[57px] text-[#17AEA4]">About us</h1>
+<section class="py-16 min-h-screen">
+	<div class="relative">
+		<div class="container">
+			<h1 class="text-center text-[50px] font-medium leading-[57px] text-[#17AEA4] mb-14">
+				About us
+			</h1>
+			<img
+				class="hidden md:block absolute max-w-lg xl:max-w-full right-0 -top-10 z-[-1]"
+				src="/bg1.png"
+				alt="bg"
+			/>
+			<img
+				class="hidden md:block absolute max-w-lg xl:max-w-full left-0 top-0 z-[-1]"
+				src="/bg2.png"
+				alt="bg"
+			/>
+			<div
+				class="border-[3px] rounded-[20px] border-primary box-border text-center flex items-center justify-center"
+			>
+				<div class="py-[72px] max-w-[938px]">
+					<h2 class="text-[25px] font-bold leading-[29px] text-secondary mb-6">
+						A fast and simple URl shortener
+					</h2>
+					<span class="w-20 h-[3px] bg-secondary block mb-5 mx-auto" />
+					<p class="text-[22px] font-normal leading-[42px] text-secondary">
+						Free URL Shortener for transforming long,ugly links into nice, memorable and trackable
+						short URLs. Use it to shorten links for any social media platforms. blogs, SMS, emails,
+						ads, or pretty much anywhere else you want to share them. Twitter, Facebook, Youtube,
+						instagram, WhatsApp, emails, SMS,videos.
+					</p>
+				</div>
+			</div>
+		</div>
 	</div>
 </section>
